8dccf23932229933d711aff3084c5343e1336275
felixem <felixem>
2015-04-04 02:31:39 +0200

    Programando el site master

--- projects/DSSGen/WebApplication2/Site.Master.cs
+++ projects/DSSGen/WebApplication2/Site.Master.cs
@@ -5,13 +5,36 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Fachadas.WebUtilities;
+
 namespace WebApplication2
 {
     public partial class SiteMaster : System.Web.UI.MasterPage
     {
+        MySession sesion;
         protected void Page_Load(object sender, EventArgs e)
         {
+            sesion = MySession.Current;
+            if (!IsPostBack)
+            {
+            }
+        }
 
+        //Visibilidad de los controles
+        private void VisibilidadControles()
+        {
+            if (sesion.IsLoged())
+            {
+                user_image.Visible = true;
+                user_label.Visible = true;
+                button_desloguear.Visible = true;
+            }
+            else
+            {
+                user_image.Visible = false;
+                user_label.Visible = false;
+                button_desloguear.Visible = false;
+            }
         }
     }
 }
