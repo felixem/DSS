9509522889db430bfe78724ea2eafa7202ef29f0
jlt17 <giovanni.lt93@gmail.com>
2015-04-13 22:30:19 +0200

    CambiarPass
    Faltaban cambios en el anterior commit

--- projects/DSSGen/DSSGenNHibernate/CEN/Moodle/UsuarioCEN_changePassword.cs
+++ projects/DSSGen/DSSGenNHibernate/CEN/Moodle/UsuarioCEN_changePassword.cs
@@ -17,22 +17,31 @@ public partial class UsuarioCEN
 public bool ChangePassword (string usuario, string pass, string newpass)
 {
         /*PROTECTED REGION ID(DSSGenNHibernate.CEN.Moodle_Usuario_changePassword) ENABLED START*/
-
+    try
+    {
         // Write here your custom code...
         bool result = false;
-        UsuarioEN us = _IUsuarioCAD.ReadOID (usuario);
+        UsuarioEN us = _IUsuarioCAD.ReadOID(usuario);
 
         //Comprobar si existe el usuario
         if (us == null)
-                return false;
+            return false;
 
         //Comparar contrase�as
-        if (Auxiliar.Encrypter.Verificar (pass, us.Password)) {
-                us.Password = newpass;
-                result = true;
+        if (Auxiliar.Encrypter.Verificar(pass, us.Password))
+        {
+            us.Password = newpass;
+            UsuarioCEN Modificar = new UsuarioCEN();
+            Modificar.Modify(us.Email, us.Dni, us.Password, us.Nombre, us.Apellidos, us.Fecha_nacimiento);
+            result = true;
         }
-
         return result;
+    }
+    catch (Exception ex) {
+        return false;
+    }
+
+        
 
 
 
