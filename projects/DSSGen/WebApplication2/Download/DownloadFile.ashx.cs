75ebeb9ea1efd178a8365c5fc0752bc1157b74cc
felixem <felixem>
2015-04-20 00:42:36 +0200

    Cambios en descargar fichero

--- projects/DSSGen/WebApplication2/Download/DownloadFile.ashx.cs
+++ projects/DSSGen/WebApplication2/Download/DownloadFile.ashx.cs
@@ -20,14 +20,23 @@ namespace DSSGenNHibernate.Download
         //Procesar petición de descarga
         public void procesarPeticionEntregaAlumno(HttpContext context)
         {
+            //Obtener el id del fichero
             System.Web.HttpRequest request = System.Web.HttpContext.Current.Request;
-            string fileName = request.QueryString[PageParameters.DescargaEntregaAlumno];
+            string id = request.QueryString[PageParameters.DescargaEntregaAlumno];
+
+            //Parámetro incorrecto
+            if (id == null)
+                throw new HttpException(404, "File not found");
+
+            //Construir el path del fichero
+            string nombreFichero = "";
+            string extension = "";
 
             System.Web.HttpResponse response = System.Web.HttpContext.Current.Response;
             response.ClearContent();
             response.Clear();
             response.ContentType = "text/plain";
-            response.AddHeader("Content-Disposition", "attachment; filename=" + fileName + ";");
+            response.AddHeader("Content-Disposition", "attachment; filename=" + nombreFichero + ";");
             response.TransmitFile(context.Server.MapPath("FileDownload.csv"));
             response.Flush();
             response.End();
